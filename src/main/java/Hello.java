682504d49cdf5855429c1d991cb9aea5f3891656
UmitUmit <umit.green@gmail.com>
2022-02-05 13:48:42 -0500

    Movie2 watching on the phone for 20 mins

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
         System.out.println("Hello World, B22");
         System.out.println("B21 future git learning");
         //movie 2 on the phone for 10 mins
+        //movie 2 on the phone for 20 mins
     }
 }
