851d61de060852e2641d07273c6c90c8a9a4be7d
UmitUmit <umit.green@gmail.com>
2022-02-05 11:52:56 -0500

    master branch, spouse changed the movie location

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,8 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World, B21");
+        System.out.println("Hello World, B22");
         System.out.println("B21 future git learning");
-        //watching more movie in bedroom
-        //ready to go back to living room
     }
 }
